a1f94f594e23504cf94c25a2c0f81ae1b59d50ac
Gttolla <lucaspedrosupergeeks@gmail.com>
2021-01-10 12:33:03 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/BEISE/HOP/Beise$HopApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.BEISE.HOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beise$HopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Beise$HopApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/BEISE/HOP/Beise$HopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.BEISE.HOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Beise$HopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
