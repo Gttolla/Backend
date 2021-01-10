351b9911c440efb5fffc3321aeb8e31fff063690
Gttolla <lucaspedrosupergeeks@gmail.com>
2021-01-10 20:08:33 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/BEISE/HOP/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperior/BEISE/HOP/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.devsuperior.BEISE.HOP.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.BEISE.HOP.dto.OrderDTO;
 import com.devsuperior.BEISE.HOP.sevices.OrderService;
@@ -22,6 +26,16 @@ public class OrderController {
 	public ResponseEntity<List<OrderDTO>> findAll() {
 		List<OrderDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
+				
 	}
 	
 }
--- backend/src/main/java/com/devsuperior/BEISE/HOP/sevices/OrderService.java
+++ backend/src/main/java/com/devsuperior/BEISE/HOP/sevices/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.BEISE.HOP.sevices;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.BEISE.HOP.dto.OrderDTO;
+import com.devsuperior.BEISE.HOP.dto.ProductDTO;
 import com.devsuperior.BEISE.HOP.entities.Order;
+import com.devsuperior.BEISE.HOP.entities.OrderStatus;
+import com.devsuperior.BEISE.HOP.entities.Product;
 import com.devsuperior.BEISE.HOP.repositories.OrderRepository;
+import com.devsuperior.BEISE.HOP.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,11 +22,24 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll(){
 		List<Order> list = repository.findOrdersWhithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
-		
 	}
-
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), 
+				Instant.now(), OrderStatus.PENDING);
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);
+	}
 }
